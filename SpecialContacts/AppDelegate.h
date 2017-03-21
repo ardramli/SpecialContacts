acd01fb11d36f4bf4c7bfbbe73b6e054649bfea8
ardMac <adliramli88@gmail.com>
2017-03-21 13:59:05 +0800

    Initial Commit

---
+++ SpecialContacts/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SpecialContacts
+//
+//  Created by ardMac on 21/03/2017.
+//  Copyright © 2017 ardMac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SpecialContacts/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SpecialContacts
+//
+//  Created by ardMac on 21/03/2017.
+//  Copyright © 2017 ardMac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
